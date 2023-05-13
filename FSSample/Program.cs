54cd6765a880dc5291bb8bdf0fc7a7def6622434
farshid jahanmanesh <farshidjahanmanesh@outlook.com>
2023-05-13 15:24:42 +0330

    some remove

--- FSSample/Program.cs
+++
@@ -1,9 +0,0 @@
-using FastEndpoints;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddFastEndpoints();
-
-var app = builder.Build();
-app.UseAuthorization();
-app.UseFastEndpoints();
-app.Run();
